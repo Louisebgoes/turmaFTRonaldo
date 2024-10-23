6db9649621ac5439fa1f777b8a52a0956c38ca89
Louisebgoes <115102707+Louisebgoes@users.noreply.github.com>
2024-10-23 20:25:04 +0000

    kkk

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,12 +8,9 @@ async function visualizarInformacoesGlobais() {
 
 const paragrafo = document.createElement('p')
 paragrafo.classList.add('graficos-container__texto')
-paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${total_pessoas_com_acesso_a_educacao} </span> estão conectadas. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
-    const container = document.getElementById('graficos-container')
-    container.appendChild(paragrafo);
+paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_empregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
+const container = document.getElementById('graficos-container')
+container.appendChild(paragrafo);
 
-
-
-vizualizarInformacoesGlobais()
 
 visualizarInformacoesGlobais()
