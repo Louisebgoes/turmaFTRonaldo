1b1c0340e4bad3c8eb67d554b1cfb924efb6f183
Louisebgoes <115102707+Louisebgoes@users.noreply.github.com>
2024-10-23 20:20:27 +0000

    adicionando concatenação

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,4 +6,14 @@ async function visualizarInformacoesGlobais() {
     console.log(dados)
 }
 
+const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container__texto')
+paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${total_pessoas_com_acesso_a_educacao} </span> estão conectadas. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo);
+
+
+
+vizualizarInformacoesGlobais()
+
 visualizarInformacoesGlobais()
